9f021b38aacdccd594c89f55fa520b5fc886e068
hyejin920 <gowks12@gmail.com>
2018-12-10 16:37:41 +0900

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,44 @@
+
+# 치환문의 예
+a= 1
+b=a+1
+print(a, b, sep='_____' )
+
+
+# 변수값 할당 오류
+# 1+ a = c
+
+
+# 세미콜로능로 치환문을 구분할 수 있다
+e=3.5; f=5.4
+print(e,f)
+
+e,f = 3.5 ,3.6
+print(e,f)
+
+x=y=z=10
+print(x,y,z)
+
+e,f=f,e
+print(e,f)
+
+a=1
+print(type(a))
+a="hello"
+print(type(a))
+
+
+
+# 확장 치환
+
+a= 10
+a+=10
+print(a)
+
+a-=3
+print (a)
+
+
+
+
+
---
+++ variable.py
@@ -0,0 +1,21 @@
+# variable.py 변수이름은 문자, 숫자 , _ 로 구성된다.
+import keyword
+
+friend = 1
+a = 10
+my_name = '김혜진'
+_yourname='둘리'
+memeber1="도우넛"
+long_string =  """
+첫번쨰라인
+두번쨰라인 긴 문자열에 사용
+"""
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+
+# 한글 이름의 변수도 가능하다
+가격1 = 2000
+print(가격1-200)
+
+
+
